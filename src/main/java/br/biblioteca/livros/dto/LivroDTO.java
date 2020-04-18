ec2dfa304e0244994a733e7a810ebb7459e2694d
Joao Pedro <piccinatoalves@hotmail.com>
2020-04-18 11:27:35 -0300

    api and templantes

---
+++ src/main/java/br/biblioteca/livros/dto/LivroDTO.java
@@ -0,0 +1,36 @@
+package br.biblioteca.livros.dto;
+
+public class LivroDTO {
+
+	private String titulo;
+	
+	private int paginas;
+	
+	private String autor;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getPaginas() {
+		return paginas;
+	}
+
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	
+}
